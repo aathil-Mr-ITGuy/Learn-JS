38bad1b773237fa2097b2137a02c7539f3476482
Aathil Ahamed <ibnusafina@gmail.com>
2020-07-10 15:03:45 +0530

    Caesars Ciphe
    One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
    
    A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
    
    Write a function which takes a ROT13 encoded string as input and returns a decoded string.
    
    All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.

---
+++ caesarsCipher.js
@@ -0,0 +1,12 @@
+function rot13(str) {
+  const input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const output    = 'NOPQRSTUVWXYZABCDEFGHIJKLM';
+
+  const index = x => input.indexOf(x);
+  const translate = x => index(x) > -1 ? output[index(x)] : x;
+  return str.split('').map(translate).join('');
+
+  
+}
+
+rot13("SERR PBQR PNZC");
